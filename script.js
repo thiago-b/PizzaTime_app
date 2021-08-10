879790924a85bcb97a530041f152a30a599b97d8
Thiago Barros <thiago.barros2211@gmail.com>
2021-08-09 23:32:39 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+    let inputHomens = document.getElementById("homens");
+    let inputMulheres = document.getElementById("mulheres");
+    let inputCriancas = document.getElementById("criancas");
+    let inputDuracao = document.getElementById("duracao");
+
+    let resultado = document.getElementById("resultado");
+
+    function calcular(){
+        console.log("Calculando...")
+        let homens = inputHomens.value;
+        let mulheres = inputMulheres.value;
+        let criancas = inputCriancas.value;
+        let duracao = inputDuracao.value;
+        let adultos = parseInt(homens) + parseInt(mulheres);
+
+        let qntdPedacos = homens * PedacosHomens(duracao) + mulheres * PedacosMulheres(duracao) + criancas * PedacosCriancas(duracao);
+        let qntdRefri = adultos * refriPP(duracao) + (criancas * refriPP(duracao)/2);
+        console.log(adultos)
+
+        resultado.innerHTML = `<p>${Math.ceil(qntdPedacos / 8)} Pizzas grandes </p>`
+        resultado.innerHTML += `<p>${Math.ceil(qntdRefri / 2000)} garrafas de refrigerante 2L </p>`
+        
+    }
+    
+    function PedacosHomens(duracao){
+        if(duracao >= 3){
+            return 6;
+        }
+        else{
+            return 4;
+        }
+    }
+
+    function PedacosMulheres(duracao){
+        if(duracao >= 3){
+            return 5;
+        }
+        else{
+            return 3;
+        }
+    }
+
+    function PedacosCriancas(duracao){
+        if(duracao >= 3){
+            return 4;
+        }
+        else{
+            return 2;
+        }
+    }
+
+    function refriPP(duracao){
+        if(duracao >= 3){
+            return 800;
+        }
+        else{
+            return 600;
+        }
+    }
+
+    
